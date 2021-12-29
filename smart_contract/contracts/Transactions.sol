278a687d15d598e6754da3b608faf30ad4f456fa
corneliajohnson <corneliaj91@gmail.com>
2021-12-29 13:57:28 -0600

    created smart contract

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -1,4 +1,70 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract Tranactions {}
+contract Tranactions {
+    uint256 transactionCount;
+    //event allow you to write data on the block change
+    event Transfer(
+        address from,
+        address receiver,
+        uint256 amount,
+        string message,
+        uint256 timestamp,
+        string keyword
+    );
+    struct TransferStruct {
+        address sender;
+        address reciver;
+        uint256 amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    //an array of TransferStructs
+    TransferStruct[] transactions;
+
+    function addToBlockchain(
+        address payable reciever,
+        uint256 amount,
+        string memory message,
+        string memory keyword
+    ) public {
+        //increment tranaction count
+        transactionCount += 1;
+
+        //add the tranaction to the tranactions array
+        transactions.push(
+            TransferStruct(
+                msg.sender,
+                reciever,
+                amount,
+                message,
+                block.timestamp,
+                keyword
+            )
+        );
+
+        //emit event
+        emit Transfer(
+            msg.sender,
+            reciever,
+            amount,
+            message,
+            block.timestamp,
+            keyword
+        );
+    }
+
+    function getAllTransactions()
+        public
+        view
+        returns (TransferStruct[] memory)
+    {
+        return transactions;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCount;
+    }
+}
